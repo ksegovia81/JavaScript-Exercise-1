41e8d568ff7317e43d9fa0f2c95b9fe80ba801fb
ksegovia81 <segovia.katherine@gmail.com>
2024-10-03 18:02:53 -0400

    Add files via upload

---
+++ Javascript files KSK/script.js
@@ -0,0 +1,21 @@
+// Definición de variables
+let nombre = "Juan";
+let edad = 25;
+let esEstudianteProgramacion = true;
+
+// Impresión en consola
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("¿Es estudiante de programación?:", esEstudianteProgramacion);
+
+// Tipos de datos
+console.log("\nTipos de datos:");
+console.log("Tipo de 'nombre':", typeof nombre);
+console.log("Tipo de 'edad':", typeof edad);
+console.log("Tipo de 'esEstudianteProgramacion':", typeof esEstudianteProgramacion);
+
+/*tipos de datos utilizados:
+nombre: Es de tipo string (cadena de texto). Se usa para almacenar texto.
+edad: Es de tipo number (número). Se utiliza para almacenar valores numéricos, en este caso un número entero.
+esEstudianteProgramacion: Es de tipo boolean (booleano). Se usa para representar valores de verdadero o falso.
+*/
